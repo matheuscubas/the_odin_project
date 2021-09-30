5b4009e6dcf809b9da37352422a14526d62a61a7
Ryuukhi <maatheusviidal@hotmail.com>
2021-09-29 23:43:30 -0300

    Finished proejct

--- ruby-projects/Caesar's_Cipher/Caesar's_Cipher.rb
+++ ruby-projects/Caesar's_Cipher/Caesar's_Cipher.rb
@@ -1,4 +1,27 @@
-def caesars_cipher ("string", number)
-  code = "string"
-  key = number
+def caesars_cipher(string, number)
+  capital_letters_range = (65..90).to_a
+  letters_range = (97..122).to_a
+
+  shift_factor = number.abs
+  message = string.bytes
+  shifted = []
+
+  shifted = message.map do |ascii|
+    if ascii.between?(65,90)
+      ascii + shift_factor > 90 ? (ascii + shift_factor) - 26 : ascii + shift_factor
+    else ascii.between?(97,122)
+      ascii + shift_factor > 122 ? (ascii + shift_factor) - 26 : ascii + shift_factor
+    end
+  end
+  encrypted_message = shifted.pack('c*')
+  return encrypted_message
+end
+
+print "Please insert a message you would like to encrypt : "
+string = gets.chomp.to_str
+
+print "Enter a security number, the security number must be a number between 1 to 26: "
+number = gets.chomp.to_i
+
+caesars_cipher(string,number)
   
