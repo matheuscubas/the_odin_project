7070b8c0e90192ee9fa5bfd071eaea6a66411b90
Ryuukhi <maatheusviidal@hotmail.com>
2021-10-17 22:09:17 -0300

    minor typo corrections

--- ruby-projects/tic_tac_toe/tic_tac_toe.rb
+++ ruby-projects/tic_tac_toe/tic_tac_toe.rb
@@ -1,6 +1,6 @@
 class Board
   TTT_BOARD = Array.new(9, '') { |i| i + 1 }
-  WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]].freeze
+  WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8],[6,4,2]]
 
   attr_accessor :board
   def initialize
@@ -37,7 +37,6 @@ p1 = Player.new('p1')
 p2 = Player.new('p2')
 board = Board.new
 game_over = false
-turn = 0
 
 puts 'TIC-TAC-TOE'.center(40)
 puts "You're playing a tic-tac-toe game, developed by Matheus Vidal"
