03d9d49dc02c36b708bda45b079bca63534f9d10
Ryuukhi <maatheusviidal@hotmail.com>
2021-10-20 22:10:35 -0300

    fixed some typos, and included replay loop

--- ruby-projects/tic_tac_toe/tic_tac_toe.rb
+++ ruby-projects/tic_tac_toe/tic_tac_toe.rb
@@ -1,10 +1,9 @@
 class Board
-  TTT_BOARD = Array.new(9, '') { |i| i + 1 }
   WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8],[2,4,6]]
 
   attr_accessor :board, :p1_combos, :p2_combos, :p1_moves, :p2_moves
   def initialize
-    @board = TTT_BOARD
+    @board = [1,2,3,4,5,6,7,8,9]
     @p1_moves = []
     @p1_combos = []
     @p2_moves = []
@@ -16,6 +15,12 @@ class Board
     puts " #{board[0]}|#{board[1]}|#{board[2]}".center(38)
     puts " #{board[3]}|#{board[4]}|#{board[5]}".center(38)
     puts " #{board[6]}|#{board[7]}|#{board[8]}".center(38)
+  end
+
+  def replay
+    @board = [1,2,3,4,5,6,7,8,9]
+    @p1_moves = []
+    @p2_moves = []
   end
 
   def add_move(position, mark)
@@ -42,9 +47,7 @@ class Board
 
   def p1_won?
     @p1_combos.each do |combo|
-      p combo
       if @win_combo.sort.any?(combo.sort)
-        p true
         return true
       end
     end
@@ -70,54 +73,71 @@ p1 = Player.new('p1')
 p2 = Player.new('p2')
 board = Board.new
 game_over = false
+replay = true
 
-puts 'TIC-TAC-TOE'.center(40)
-puts "You're playing a tic-tac-toe game, developed by Matheus Vidal"
-puts "This is a two player game, please insert player number one's name: "
-p1.name = gets.chomp
-puts "Now please insert player number two's name: "
-p2.name = gets.chomp
-puts "All set #{p1.name} and #{p2.name} enjoy the game!"
-board.print_game_board
-puts "Each player must select a position inside the board, and try to make a straight line to win. \nnone of the players are allowed to overtake the other player position"
-
-while game_over == false
-  if board.draw?(board) == false
-    puts "It's a Draw!"
-    game_over = true
-    break
-  end
-  puts "#{p1.name} what is your move?"
-  position = gets.chomp.to_i
-  while board.valid_move?(position) == false
-    puts "Please #{p1.name} select another position"
-    position = gets.chomp.to_i
-  end
-  board.p1_moves(position)
-  board.add_move(position, 'X')
+while replay == true
+  puts 'TIC-TAC-TOE'.center(40)
+  puts "You're playing a tic-tac-toe game, developed by Matheus Vidal"
+  puts "This is a two player game, please insert player number one's name: "
+  p1.name = gets.chomp
+  puts "Now please insert player number two's name: "
+  p2.name = gets.chomp
+  puts "All set #{p1.name} and #{p2.name} enjoy the game!"
   board.print_game_board
-  if board.p1_won? == true
-    puts "Congratulations #{p1.name} you are the winner!"
-    game_over = true
-    break
-  end
-  if board.draw?(board) == false
-    puts "It's a Draw!"
-    game_over = true
-    break
-  end
-  puts "#{p2.name} what's your move?"
-  position = gets.chomp.to_i
-  while board.valid_move?(position) == false
-    puts "Please #{p2.name} select another position"
+  puts "Each player must select a position inside the board, and try to make a straight line to win. \nnone of the players are allowed to overtake the other player position"
+  while game_over == false
+    if board.draw?(board) == false
+      puts "It's a Draw!"
+      game_over = true
+      break
+    end
+    puts "#{p1.name} what is your move?"
     position = gets.chomp.to_i
+    while board.valid_move?(position) == false
+      puts "Please #{p1.name} select another position"
+      position = gets.chomp.to_i
+    end
+    board.p1_moves(position)
+    board.add_move(position, 'X')
+    board.print_game_board
+    if board.p1_won? == true
+      puts "Congratulations #{p1.name} you are the winner!"
+      game_over = true
+      break
+    end
+    if board.draw?(board) == false
+      puts "It's a Draw!"
+      game_over = true
+      break
+    end
+    puts "#{p2.name} what's your move?"
+    position = gets.chomp.to_i
+    while board.valid_move?(position) == false
+      puts "Please #{p2.name} select another position"
+      position = gets.chomp.to_i
+    end
+    board.p2_moves(position)
+    board.add_move(position, 'O')
+    board.print_game_board
+    if board.p2_won? == true
+      game_over = true
+      p "Congratulations #{p2.name} you are the winner!"
+      break
+    end
   end
-  board.p2_moves(position)
-  board.add_move(position, 'O')
-  board.print_game_board
-  if board.p2_won? == true
-    game_over = true
-    p "Congratulations #{p2.name} you are the winner!"
+    puts "That was a nice game, if do you wanna play again, press Y if you don't press N"
+    answer = gets.chomp.to_s.upcase
+    if answer == "Y"
+      replay = true
+      game_over = false
+      board.replay
+    elsif answer == "N"
+      return replay = false
+      break
+    else puts "Please answer only with Y or N, Do you wanna play again? Y/N"
+      answer = gets.chomp.to_s.upcase
+    end
+  if replay == false
     break
   end
 end
